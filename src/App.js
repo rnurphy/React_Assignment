bb665991fae717344dc03d82f1c239e6e3e3509b
code1218 <skjil1218@gmail.com>
2024-03-07 02:24:26 +0900

    문제 등록

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import RootLayout from './components/RootLayout/RootLayout';
+import MainSidebarBottom from './components/MainSidebarBottom/MainSidebarBottom';
+import RootHeader from './components/RootHeader/RootHeader';
+import { SIDEBAR_MENUS } from './constants/menu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RootLayout>
+      <RootHeader />
+      <Routes>
+        {
+          SIDEBAR_MENUS.map(
+            menu => 
+            <Route key={menu.id} path={menu.path} element={menu.element}/>
+          )
+        }
+      </Routes>
+      <MainSidebarBottom />
+    </RootLayout>
   );
 }
 
---
+++ src/pages/PhotoAlbum/PhotoAlbum.jsx
@@ -0,0 +1,17 @@
+
+/**
+ *  1. 사진 등록하기를 통해 등록된 이미지들을 각자 자유롭게 디자인하여 불러와야함.
+ *  2. localStorage에 저장된 사진이 없으면 
+ *      <h1>불러올 사진이 없습니다.<h1>
+ *      문구가 중앙에 나오도록해야함.
+ */
+
+function PhotoAlbum() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default PhotoAlbum;
